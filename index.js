77791eb1ffdcd51bbfae19d2b80fc617df1a494f
p1995 <pj1311995@gmail.com>
2019-05-15 14:08:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+function changeFavicon() {
+var link;
+link = document. createElement('link');
+link. type = 'image/x-icon';
+link. rel = 'shortcut icon';
+link. href = 'http://www.stackoverflow.com/favicon.ico';
+document. getElementsByTagName('head')[0]. appendChild(link);
+};
+
+changeFavicon()
 document.getElementById("button").addEventListener("click", e => {
   e.preventDefault();
   var username = document.getElementsByClassName("inputtext")[0].value;
